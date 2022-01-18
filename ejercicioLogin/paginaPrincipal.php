0b5b0e3a85fe6c3b30a9dd4ce4e6dbb2b62f276d
vitropez <64587299+vitropez@users.noreply.github.com>
2022-01-18 19:58:57 +0100

    Add files via upload

---
+++ ejercicioLogin/paginaPrincipal.php
@@ -0,0 +1,30 @@
+
+
+
+
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Pagina Principal</title>
+		<link href="estilos.css" rel="stylesheet" type="text/css">
+	</head>
+		
+	
+	<body>
+	
+	
+		<h2>Bienvenido</h2> 
+		
+		
+		<form method="post" action="index.php">
+		<input type="hidden" name="cerrar" value="uno">
+		
+		<button type="submit">Salir</button>
+		
+		
+		
+		</form>
+	
+	</body>
+</html>
